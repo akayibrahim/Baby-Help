898fc8b3a7721ac9f977de17bd5ad23f19e705e2
iakay <that_see@hotmail.com>
2021-02-15 00:57:32 +0300

    helps query fix

--- routes/helps.js
+++ routes/helps.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 	// SELECT * FROM helps WHERE week like "%" || (SELECT CONCAT(TIMESTAMPDIFF(WEEK, u.birthDate, NOW()) + 1, ";") FROM users u where id=25) || "%" or month like "%" || (SELECT CONCAT(TIMESTAMPDIFF(MONTH, u.birthDate, NOW()) + 1, ";") FROM users u where id=25) || "%"
 	// SELECT * FROM helps WHERE week like CONCAT("%", CONCAT((SELECT CONCAT(TIMESTAMPDIFF(WEEK, u.birthDate, NOW()) + 1, ";") FROM users u where id=25), "%")) or month like CONCAT("%", CONCAT((SELECT CONCAT(TIMESTAMPDIFF(MONTH, u.birthDate, NOW()) + 1, ";") FROM users u where id=25), "%"))
-	const query = 'SELECT * FROM helps WHERE week like CONCAT("%", CONCAT((SELECT CONCAT(TIMESTAMPDIFF(WEEK, u.birthDate, NOW()) + 1, ";") FROM users u where id='+req.query.id+'), "%")) or month like CONCAT("%", CONCAT((SELECT CONCAT(TIMESTAMPDIFF(MONTH, u.birthDate, NOW()) + 1, ";") FROM users u where id='+req.query.id+'), "%"))';
+	const query = 'SELECT * FROM helps WHERE week like CONCAT("%;", CONCAT((SELECT CONCAT(TIMESTAMPDIFF(WEEK, u.birthDate, NOW()) + 1, ";") FROM users u where id='+req.query.id+'), "%")) or month like CONCAT("%;", CONCAT((SELECT CONCAT(TIMESTAMPDIFF(MONTH, u.birthDate, NOW()) + 1, ";") FROM users u where id='+req.query.id+'), "%"))';
 	connection.query(query, function (error, results, fields) {
 	  	if(error){
 	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
