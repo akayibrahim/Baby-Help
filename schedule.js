7536f386c5a29678ebeefa84cfd1b92ae024bea9
iakay <that_see@hotmail.com>
2021-02-18 00:08:26 +0300

    find unread help on board for notify

--- schedule.js
+++ schedule.js
@@ -1,8 +1,11 @@
 const cronJob = require('cron').CronJob;
 var mysql= require('mysql');
 
-// process.env['MYSQL_HOST_IP'] = 'localhost';
-// process.env['MYSQL_HOST_PORT'] = '6603';
+ process.env['MYSQL_HOST_IP'] = 'localhost';
+ process.env['MYSQL_HOST_PORT'] = '6603';
+
+ // SELECT * FROM helps WHERE ID IN ( SELECT b.ID FROM users a, helps b WHERE a.ID = 17 AND !FIND_IN_SET(b.ID, REPLACE(a.READ, ';', ',') ) ) AND ( WEEK LIKE CONCAT( "%;", CONCAT( ( SELECT CONCAT( TIMESTAMPDIFF(WEEK, u.birthDate, NOW()) + 1, ";") FROM users u WHERE id = 17 ), "%" ) ) OR MONTH LIKE CONCAT( "%;", CONCAT( ( SELECT CONCAT( TIMESTAMPDIFF(MONTH, u.birthDate, NOW()) + 1, ";") FROM users u WHERE id = 17 ), "%" ) ) ) LIMIT 1
+
 
 const job = new cronJob('*/5 * * * * *', () => {
     global.connection = mysql.createConnection({ // global.connection = mysql.createPool({
@@ -21,9 +24,18 @@ const job = new cronJob('*/5 * * * * *', () => {
         if (error) {
             console.log(error);
         } else {
-            console.log(results);
+            // console.log(results);
             results.forEach(result => {
-                // console.log(result.name);
+                console.log(result.id);
+                connection.query('SELECT * FROM helps WHERE ID IN ( SELECT b.ID FROM users a, helps b WHERE a.ID = '+result.id+' AND !FIND_IN_SET(b.ID, REPLACE(a.READ, ";", ",") ) ) AND ( week like CONCAT("%;", CONCAT((SELECT CONCAT(TIMESTAMPDIFF(WEEK, u.birthDate, NOW()) + 1, ";") FROM users u where id='+result.id+'), "%")) or month like CONCAT("%;", CONCAT((SELECT CONCAT(TIMESTAMPDIFF(MONTH, u.birthDate, NOW()) + 1, ";") FROM users u where id='+result.id+'), "%")) )', function(helpError, helpResults, helpFields) {
+                    if (helpError) {
+                        console.log(helpError);
+                    } else {
+                        helpResults.forEach(helpResult => {
+                            console.log(helpResult.label);
+                        });
+                    }
+                });     
             });
         }
     });
