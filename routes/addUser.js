eb4153118307064d81a2bada7eacfe8e643807ae
iakay <that_see@hotmail.com>
2021-02-21 23:13:11 +0300

    push integration

--- routes/addUser.js
+++ routes/addUser.js
@@ -8,8 +8,9 @@ router.post('/', function(req, res, next) {
 	var birthDate = req.body.birthDate;
 	var name = req.body.name;
 	var language = req.body.language;
+	var pushToken = req.body.pushToken;
 
-	global.connection.query('INSERT INTO users (email, sex, name, birthDate, createDate, language) VALUES ("'+email+'","'+sex+'","'+name+'","'+birthDate+'", now(), "'+language+'")', function (error, results, fields) {
+	global.connection.query('INSERT INTO users (email, sex, name, birthDate, createDate, language, pushToken) VALUES ("'+email+'","'+sex+'","'+name+'","'+birthDate+'", now(), "'+language+'", "'+pushToken+'")', function (error, results, fields) {
 		if(error){
 			res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
 			//If there is error, we send the error in the error section with 500 status
--- schedule.js
+++ schedule.js
@@ -1,13 +1,17 @@
 const cronJob = require('cron').CronJob;
 var mysql= require('mysql');
+const { Expo } = require('expo-server-sdk');
 
 // process.env['MYSQL_HOST_IP'] = 'localhost';
 // process.env['MYSQL_HOST_PORT'] = '6603';
 
  // SELECT * FROM helps WHERE ID IN ( SELECT b.ID FROM users a, helps b WHERE a.ID = 17 AND !FIND_IN_SET(b.ID, REPLACE(a.READ, ';', ',') ) ) AND ( WEEK LIKE CONCAT( "%;", CONCAT( ( SELECT CONCAT( TIMESTAMPDIFF(WEEK, u.birthDate, NOW()) + 1, ";") FROM users u WHERE id = 17 ), "%" ) ) OR MONTH LIKE CONCAT( "%;", CONCAT( ( SELECT CONCAT( TIMESTAMPDIFF(MONTH, u.birthDate, NOW()) + 1, ";") FROM users u WHERE id = 17 ), "%" ) ) ) LIMIT 1
 
+let expo = new Expo({ accessToken: process.env.EXPO_ACCESS_TOKEN });
 
-const job = new cronJob('*/5 * 10 * * *', () => {
+const job = new cronJob('*/5 * 10 * * *', () => {    
+    sendPush();
+    /*
     var pool = mysql.createPool({ // global.connection = mysql.createPool({
         connectionLimit : 10,
         host     : process.env.MYSQL_HOST_IP,
@@ -41,6 +45,36 @@ const job = new cronJob('*/5 * 10 * * *', () => {
         });
       });
     console.log('This will get executed every 5 seconds');
+    */
 }, null, true, 'Europe/London');
+
+const sendPush = () => {
+    let messages = [];
+    let pushToken = 'ExponentPushToken[kTcgYUJs0n1VaPBOuZd8xp]';
+    if (!Expo.isExpoPushToken(pushToken)) {
+        console.error(`Push token ${pushToken} is not a valid Expo push token`);    
+        return
+    }
+    messages.push({
+        to: pushToken,
+        sound: 'default',
+        body: 'This is a test notification',
+        data: { withSome: 'data' },
+    })
+    const chunks = expo.chunkPushNotifications(messages);
+    (async () => {
+        // Send the chunks to the Expo push notification service. There are
+        // different strategies you could use. A simple one is to send one chunk at a
+        // time, which nicely spreads the load out over time:
+        for (let chunk of chunks) {
+          try {
+            let receipts = await expo.sendPushNotificationsAsync(chunk);
+            console.log(receipts);
+          } catch (error) {
+            console.error(error);
+          }
+        }
+      })();
+}
 
 job.start();
